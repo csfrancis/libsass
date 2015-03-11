1a7e10f5de31c2e42134ef15a4385d35c40aa840
Scott Francis <scott.francis@shopify.com>
2015-03-11 18:02:15 +0000

    Add -std=gnu++11 to CXXFLAGS

--- extconf.rb
+++ extconf.rb
@@ -2,5 +2,6 @@ require 'mkmf'
 # .. more stuff
 #$LIBPATH.push(Config::CONFIG['libdir'])
 $CFLAGS << " #{ENV["CFLAGS"]}"
+$CXXFLAGS << " -std=gnu++11"
 $LIBS << " #{ENV["LIBS"]}"
 create_makefile("libsass")
